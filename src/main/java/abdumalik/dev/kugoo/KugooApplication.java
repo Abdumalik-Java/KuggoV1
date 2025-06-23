59f15b10ff8a1aa2c0107d4615b7f43c2e116f23
Abdumalik <abdumalikbakhtiyorovwork@gmail.com>
2025-06-23 16:28:56 +0500

    Project started successfully by Abdumalik-Java

---
+++ src/main/java/abdumalik/dev/kugoo/KugooApplication.java
@@ -0,0 +1,13 @@
+package abdumalik.dev.kugoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KugooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KugooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/abdumalik/dev/kugoo/KugooApplicationTests.java
@@ -0,0 +1,13 @@
+package abdumalik.dev.kugoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KugooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
